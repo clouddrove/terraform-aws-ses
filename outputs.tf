c96247b7434a8d6606b7cf0ecb8d9114836eb2b7
theprashantyadav <theprashantyadav7@gmail.com>
2023-10-23 14:47:53 +0530

    feat: added iam output

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,21 @@
-# Module      : DOMAIN IDENTITY
-# Description : Terraform module to create domain identity using domain
-#output "domain_identity_arn" {
-#  value       = aws_ses_domain_identity.main[0].arn
-#  description = "ARN of the SES domain identity."
-#}
+##Module      : DOMAIN IDENTITY
+## Description : Terraform module to create domain identity using domain
+output "domain_identity_arn" {
+  value       = aws_ses_domain_identity.default[0].arn
+  description = "ARN of the SES domain identity."
+}
 
-#output "id" {
-#  value       = try(aws_ses_domain_identity_verification.default[0].id, "")
-#  description = "The domain name of the domain identity."
-#}
+output "id" {
+  value       = try(aws_ses_domain_identity_verification.default[0].id, "")
+  description = "The domain name of the domain identity."
+}
+
+output "iam_access_key_secret" {
+  description = "The access key secret."
+  value       = try(aws_iam_access_key.default[0].secret, "")
+  sensitive   = true
+}
+output "iam_access_key_id" {
+  description = "The access key ID."
+  value       = try(aws_iam_access_key.default[0].id, "")
+}
