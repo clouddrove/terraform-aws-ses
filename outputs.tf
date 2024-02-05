c4239beeeee600568474c2c01aa052a5853007cb
Anmol Nagpal <anmol@clouddrove.com>
2024-02-05 16:30:09 +0530

    fix: fix domain identity not found in output issue

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 ##Module      : DOMAIN IDENTITY
 ## Description : Terraform module to create domain identity using domain
 output "domain_identity_arn" {
-  value       = aws_ses_domain_identity.default[0].arn
+  value       = try(aws_ses_domain_identity.default[0].arn, "")
   description = "ARN of the SES domain identity."
 }
 
